7e4a25c1050115da44ad4a83b4efbaef7355db91
vijayKumarSonii <vijay.kumar@wtmails.com>
2024-07-08 13:33:14 +0530

    live

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,8 @@ import "./../public/assets/styles/customStyle.scss"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./../public/assets/styles/homepageStyle.scss"
 import "./../public/assets/styles/mixins.scss"
-import "./../public/assets/styles/aboutPageStyle.scss"
+import "./../public/assets/styles/aboutPageStyle.scss";
+import "./../App.scss"
 
 import HeaderNavbar from "./header/header";
 import Footer from "./footer/footer";
