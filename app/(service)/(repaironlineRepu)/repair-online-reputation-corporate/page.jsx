b04d5c006cb85fe1b1960a07328e464ba43eb594
vijayKumarSonii <vijay.kumar@wtmails.com>
2024-07-10 17:52:27 +0530

    online reputation service

---
+++ app/(service)/(repaironlineRepu)/repair-online-reputation-corporate/page.jsx
@@ -0,0 +1,89 @@
+'use client'
+import React from 'react'
+import InnerBanner from './../../../../common/InnerBanner'
+import ImageInfoWrap from './../../../../common/ImageInfoWrap'
+import section1Image from './../../../../public/assets/Images/services/Online-Repair-Service.jpg';
+import section2Image from './../../../../public/assets/Images/services/Manage-the-Way.webp';
+import section3Image from './../../../../public/assets/Images/services/Bury-the-Negativity.webp';
+
+
+
+import FullWidthCTASection from './../../../../common/FullWidthCTASection';
+import SectionTopInfo from './../../../../common/SectionTopInfo';
+import FAQs from './../../../../common/FAQs';
+
+
+const RepairCorporate = () => {
+
+  const faqData = [
+    {
+      question: 'Why is having a good online reputation important?',
+      answer: '<p>Any bad review online can drop your chance to build a new and strong network of customers, and you all are here to make it big in the world of brands, so you can’t risk your chances of failing online because of some negative content.</p><p>This is an internet empowered world, and we all are practically living 24*7 with cell phones in our hands. For any small query, it’s Google with turn our eyes to, so for any corporate sector that is up there to sell or showcase their products or services, it’s crucial to have a positive image online, otherwise, no one will buy your products or will rely on your company.</p><p>Anything negative out on the web can easily damage your hard-earned reputation and business quickly, and having no reputation online will by far drive your potential clients to competitors.',
+    },
+    {
+      question: 'How my reputation can be damaged?',
+      answer: '<p>There are several ways that can damage your repute easily, some of them are:</p><ul><li>Your nemesis can project something bad about your brand to keep you away from the competition.</li><li>A number of websites let the users post complaints about the brand or company, so any unhappy client can be responsible to degrade your reputation.</li><li>Bad rating on Google is one of the easiest ways to downgrade your reputation in the market, your customers can post anything bad about you online and this will instantly create havoc to your business.</li></ul>',
+    },
+    {
+      question: 'How much time does it take?',
+      answer: '<p>Online reputation management is quite a long process, depending on how bad is your image online our team creates a customized program that is adapted to drag down the negative content from various platforms and upload new and positive content and manage it to rank on the first page of SERP.</p><p>However, Google frequently updates its algorithms, so repairing your tarnished reputation will take a different approach to match up the level you want. In addition to this, you can see the viable changes within 3 months of the campaign.</p>      ',
+    }
+    // Add more FAQ items as needed
+  ];
+
+  return (
+    <div>
+      
+      <InnerBanner
+        title={"Repair Online Reputation Corporate"}
+        info={'Repair Your Disparaged Image Online by Reputation Repair and Management Services'}
+      />
+      {/* ======New Section Start ====== */}
+      <ImageInfoWrap
+        title={'Revamp your Digital Identity with the Best Online Reputation Repair Service'}
+        imageUrl={section1Image}
+        description={'<p>Internet is the thrust that is influencing billions of people, and somehow this becomes a reason that your online presence on over the web can create or rupture your identity. A constructive reputation online states the largest part about your business and is the most fundamental thing in the world today.</p><p>A majority of the clientele rely on the internet whenever it comes to obtain certain services like shopping, vacation planning, booking up a hotel, and many more. However, businesses with any bad reviews are all in the zone of falling down.</p><p>You have spent years in building your esteemed organization and certainly, an unhappy client or your rival is making it worse for you to survive. This is one of the major reasons their online reputation has been tainted because of the negative content in the search results.</p><p>Your prospects can share their point of view explicitly on different social platforms, which can make your reputation, but sometimes it can also hinder your reputation with one negative comment. Our experts assist you in reforming your stippled digital reputation, by helping you to remove the negative links online and reinstating them with good information that reflects the positive approach of your brand.</p>'}
+      />
+      {/* ======New Section Start ====== */}
+      <FullWidthCTASection
+        title={'Let our experts fix your tarnished reputation.'}
+      />
+      {/* ======New Section Start ====== */}
+      <ImageInfoWrap
+        title={'Manage the Way Your Name Appears on the Search Engine with Online Reputation Repair'}
+        imageUrl={section2Image}
+        isReverse={true}
+        description={'<p>Setting up your online image can make a gigantic difference in the opportunities that flies your way. Sounds unbelievable, well, here is a case when people see positive reviews about your business they surely want to check out what you offer, this is how you can get new opportunities to expand your business as well as get traffic to your website.</p><p>Managing online reputation has always is crucial for several brands because in many cases a negative search result on the initial page of a search engine can drop the sales of the business regardless of the credibility of the information.</p><p>Good and positive reviews on the web can boost the credibility of your company as well as will increases your market value. Positive reviews direct a good reputation that helps your prospects to trust you more, helping in increasing the revenue.</p><p>However, the internet serves as a vast environment to potential customers, as they can know easily what you offer them and how good are your products and services. With different channels to maneuver, our agency is well versed with all online reputation protection services, and we can help you to restructure your negative image</p>'}
+      />
+      {/* ======New Section Start ====== */}
+      <FullWidthCTASection
+        title={'Get rid of the deceptive and malevolent content.'}
+      />
+      {/* ======New Section Start ====== */}
+      <ImageInfoWrap
+        title={'Bury the Negativity with Online Reputation Repair Services'}
+        imageUrl={section3Image}
+        description={'<p>We smooth the progress of managing your online reputation by building your positive digital presence on different social media channels, social networking outlets, writing excellent content that encourages the readers to collaborate with you and publishing press releases and news about your latest ventures.</p><p>Most corporate sectors provide precision, and now the content befalls as a fundamental part of any marketing efforts, we can see organization resisting to maintain a good image in front of their audience. With the deceptive and fallacious content floating over the internet, it certainly becomes complex for any business to sustain a vigorous image online.</p><p>Our agency aims to repair and retain a positive reputation about you and your brand in the search engine result pages, on top of that, we channel to maintain your constructive presence over different digital marketing channels, intending to condense and purge the negative content from the search results.</p><p>Online reputation repair is one of the most preferred services by several organizations in today’s internet-empowered world, instead of self-managing the repute, most of the organizations are calling our experts to handle and repair their reputation online.</p>'}
+      />
+      {/* ======New Section Start ====== */}
+      <FullWidthCTASection
+        title={'Take charge of your online reputation with the help of experts.'}
+      />
+      {/* ======New Section Start ====== */}
+      <section className='sectionPadding'>
+        <div className="container">
+          <SectionTopInfo
+            isCenter={true}
+            isFullWidth={true}
+            smallTitle={'FAQs'}
+            title={'FAQs for Repair Online Reputation – Corporate'}
+          />
+          <FAQs faqList={faqData} />
+        </div>
+      </section>
+      {/* ======New Section Start ====== */}
+    </div>
+  )
+}
+
+export default RepairCorporate
---
+++ app/(service)/(repaironlineRepu)/repair-online-reputation-personal/page.jsx
@@ -0,0 +1,94 @@
+'use client'
+import React from 'react'
+import InnerBanner from './../../../../common/InnerBanner'
+import ImageInfoWrap from './../../../../common/ImageInfoWrap'
+import section1Image from './../../../../public/assets/Images/services/online-reputation.webp';
+import section2Image from './../../../../public/assets/Images/services/repair-online-reputation.webp';
+import section3Image from './../../../../public/assets/Images/services/Choose-strategic-approach.webp';
+
+
+
+import FullWidthCTASection from './../../../../common/FullWidthCTASection';
+import SectionTopInfo from './../../../../common/SectionTopInfo';
+import FAQs from './../../../../common/FAQs';
+
+
+
+const RepairPersonal = () => {
+
+  const faqData = [
+    {
+      question: 'How much time will it take to fix my online reputation?',
+      answer: 'It is a bit difficult to tell you the exact time due to the constantly changing algorithms of Google. However, our clients have been able to see significant improvement in three months. The average reputation repair program takes six to twelve months for satisfactory prominent results.',
+    },
+    {
+      question: 'When something appears online, will it be there forever?',
+      answer: 'Basically, yes! Usually, it is impossible to remove or delete once any content is on the web, unless you contact the admin personally and request him to delete the content, which very troublesome job, next to impossible only. Fortunately, most of the people don’t scroll beyond the first page, so you can always push the negative information off the first few pages making it get effectively invisible.',
+    },
+    {
+      question: 'How much does reputation repair cost?',
+      answer: 'Online reputation repair does not follow the one size fits all policy. We customize the strategies according to the need and demands of the client. The cost depends upon the amount of work needed to be done in fixing your image. It is also based on how much damage has been done to your reputation, how widely it is spreader and what measures needed to be taken to restore it.',
+    },
+    {
+      question: 'Can you offer any guarantee for restoring my online reputation?',
+      answer: '<p>Actually, no one can offer you a guarantee and those companies who are doing are just making fake promises. It would be very wrong and unprofessional for us to offer any guarantee to you as there are a lot of factors beyond our control, such as:</p><ul><li>Always changing search engine algorithms</li><li>Fresh content being published on target listings at regular interval</li><li>Content with more third-party links directed at it</li><li> New and negative content, PR or commentary beyond our control</li></ul><p>However, we do commit to making progress by hitting KPIs with time. Moreover, we can guarantee the creation of content and share online on your behalf at regular intervals.</p>',
+    }
+    // Add more FAQ items as needed
+  ];
+
+  return (
+    <div>
+      
+      <InnerBanner
+        title={"Repair Online Reputation Personal"}
+        info={'Shed defamation and fix your Internet image with our best Online Reputation Repair Services'}
+      />
+      {/* ======New Section Start ====== */}
+      <ImageInfoWrap
+        title={'Restore your online reputation and enhance the opportunities coming your way'}
+        imageUrl={section1Image}
+        description={'<p>Nowadays, everybody is searching for everybody else on the Internet. Your reputation is determined by your digital identity. You can be searched on the internet at any moment, by anybody and for any reason.</p><p>Bad stuff can happen to anybody at any time. It takes years to build a reputation and a single negative press article, an angry neurotic ex-lover, jealous colleague or simply put just bad luck to ruin it overnight. Even simple social media mistakes can invoke an explosion of negative comments from angry followers.</p><p>Regardless of the story about you is true or not, you will be judged by the search results. No one cares whether they are old and how much you have accomplished since then, the damage is done.</p><p>It has become important more than ever to take care of your presence on Google, and across all social networks. Like professional brands, your ‘personal’ brand can raise positive and negative reactions for which you have to deal with the consequences.</p><p>Don’t let negative content damage your search results and your image. Repair your online reputation with our agency. We provide the best online reputation services which can help you with fixing the negative image of yours.</p>'}
+      />
+      {/* ======New Section Start ====== */}
+      <FullWidthCTASection
+        title={'Struggling with negative search results?'}
+      />
+      {/* ======New Section Start ====== */}
+      <ImageInfoWrap
+        title={'Repair your online reputation with the best solutions'}
+        imageUrl={section2Image}
+        isReverse={true}
+        description={'<p>The internet is an ocean of free-for-all resources. Irrespective of what you one can always search to learn more about you. Of course negative content is not even the least you would like them to see.</p><p>Employers, Landlords, Consumers, colleagues your children, significant others are all searching you online. Negative or defamatory posts referring to you can cause damage to your future prospects. It only takes a few negative reviews to blow your personal reputation.</p><p>As an industry-leading team of reputation repair and management experts, we are the most trusted source for the removal of online negative defamatory posts. We commit our clients with a fast and efficient solution for fixing their reputation.</p><p>We have years of experience in reputation repair and management. Our team has helped Doctors, Filmmakers, Attorneys, Architects, Executives, Tax Consultants, Accountants, Celebrities, and Realtors in repairing their online negative reputation. Many of our clients have even availed of our social media reputation repair services.</p><p>It takes proper techniques and tools with a personalized strategy for a particular client for online reputation repair and management. So it’s not an easy task which can be done by anybody. Any carelessness in this matter will lead to more negative coverage and your reputation can be ruined even further.</p><p>Our trusted and strategic ORM solutions will repair your reputation and help you regain control of your name online helping you brand yourself.</p>'}
+      />
+      {/* ======New Section Start ====== */}
+      <FullWidthCTASection
+        title={'Consult our experts and start improving your online reputation today.'}
+      />
+      {/* ======New Section Start ====== */}
+      <ImageInfoWrap
+        title={'Choose a strategic approach to defend yourself online'}
+        imageUrl={section3Image}
+        description={'<p>Google is the first place people go to learn about you. Whatever is written online about you matters, as most of the people will believe it. Negative stuff can affect your whole life and can be devastating.</p><p>In this case, what you really need is professional and strategic help that can restore your online reputation. Our team of experts offers you the best online reputation repair services. We can assist you with:</p><ul><li style="font-weight: 400;" aria-level="1"> Project your right image</span></li><li style="font-weight: 400;" aria-level="1"> Get rid of negative links from the Internet&nbsp;</span></li><li style="font-weight: 400;" aria-level="1"> Create and optimize positive content</span></li><li style="font-weight: 400;" aria-level="1"> Bury off misleading information</span></li><li style="font-weight: 400;" aria-level="1"> Establish your credibility&nbsp;</span></li></ul> <p>Our experts have full knowledge and understanding of the ever-changing Google algorithms. We employ various Google reputation repair strategies and techniques to get rid of negative links.&nbsp;</p><p>Most of the companies claim that they can remove bad links from Google, remove negative press release or remove negative online content. Let us aware you that these claims are all false and misleading as it is not even possible to delete all the content.</p><p>Only a little fraction of content is under your control and only that can be deleted. We use the techniques and strategies with which can suppress all the negative links about you online by publishing positive content frequently. This will result in pushing back of the negative content from page one of Google Search results to quite further pages.&nbsp;</p><p>For online reputation repair, we focus on creating positively strong profiles by following the best SEO practices that will have a huge shot at getting ranked well over time, automatically suppressing the negative links.</p>'}
+      />
+      {/* ======New Section Start ====== */}
+      <FullWidthCTASection
+        title={'Is the negative information ranking highly in the Google search results against your name? Don’t panic!'}
+      />
+      {/* ======New Section Start ====== */}
+      <section className='sectionPadding'>
+        <div className="container">
+          <SectionTopInfo
+            isCenter={true}
+            isFullWidth={true}
+            smallTitle={'FAQs'}
+            title={'FAQ’s for Repair Online Reputation-Personal'}
+          />
+          <FAQs faqList={faqData} />
+        </div>
+      </section>
+      {/* ======New Section Start ====== */}
+    </div>
+  )
+}
+
+export default RepairPersonal
