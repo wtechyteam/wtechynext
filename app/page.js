a438619f22d7536ce5ea9123017bca7b0caee328
vijayKumarSonii <vijay.kumar@wtmails.com>
2024-07-18 16:46:20 +0530

    1234

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Homepage from './home/page';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <>
    <Homepage/>
+   <SpeedInsights/>
     </>
   );
 }
