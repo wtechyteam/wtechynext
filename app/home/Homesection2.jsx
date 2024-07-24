96ad93f12d5f836f2d9251a762ed9c6bf1d0118f
vijayKumarSonii <vijay.kumar@wtmails.com>
2024-07-24 17:18:21 +0530

    h1

--- app/home/Homesection2.jsx
+++ app/home/Homesection2.jsx
@@ -12,9 +12,9 @@ const HomeSection2 = () => {
         <div className="row align-items-center">
           <div className="col-lg-6" data-aos="fade-right" data-aos-duration="1500">
             <h4 className="section-title-sm">Amplify your Sales in 90 Days</h4>
-            <h2 className="title-xl fw-bold">
+            <h1 className="title-xl fw-bold">
               Digital Marketing Company Helping Businesses to Augment their Online Presence
-            </h2>
+            </h1>
             <p>
               We leave no leaf unturned when it comes to website optimization, enabling it to rank higher in search engines such as Google. We conduct in-depth competitor research to produce effective results within the committed time frame and budget. Each activity is targeted to ensure that your website ranks high in organic searches.
             </p>
