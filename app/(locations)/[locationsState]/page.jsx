b5d5abc0f0e0f786db205498c66cedb0ccaef3c6
Wtechy <randeep@wtmails.com>
2024-09-25 16:18:03 +0530

    contentful

--- app/(locations)/[locationsState]/page.jsx
+++ app/(locations)/[locationsState]/page.jsx
@@ -1,5 +1,7 @@
 import InnerBanner from '../../../common/InnerBanner';
 import { fetchEntries } from "../../contentful";
+import SectionTopInfo from "./../../../common/SectionTopInfo";
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'; // Import the renderer
 
 // Determine the content type based on the country in the slug
 function getContentTypeFromSlug(slug) {
@@ -66,14 +68,76 @@ export default async function LocationState({ params }) {
   return (
     <>
       <head>
-        <title>
-        {`SEO Services Company in ${location.fields.name}`}
-        </title>
+        <title>{`SEO Services Company in ${location.fields.name}`}</title>
       </head>
       <InnerBanner
         title={`SEO Services Company in ${location.fields.name}`}
         info={"We Help Your Business Soar to Greater Heights"}
       />
+      <section className="sectionPadding services">
+        <div className="container">
+          <SectionTopInfo
+            isCenter={true}
+            isFullWidth={true}
+            description={location.fields.desc} // Pass desc here
+            title={`SEO Services in ${location.fields.name}`}
+            // smallTitle={"Ready to collaborate with a 360-degree digital marketing company?"}
+            showContactBtn={false}
+          />
+        </div>
+      </section>
+
+      <section className="sectionPadding services">
+        <div className="container">
+          <SectionTopInfo
+            isCenter={true}
+            isFullWidth={true}
+            description={location.fields.desc2} // Pass desc here
+            title={`SEO Services in ${location.fields.name}`}
+            smallTitle={`Area, We Cover in ${location.fields.name}`}
+            showContactBtn={false}
+          />
+        </div>
+      </section>
+
+      <section className="sectionPadding services">
+        <div className="container">
+          <SectionTopInfo
+            isCenter={true}
+            isFullWidth={true}
+            description={location.fields.desc3} // Pass desc here
+            title={`See How Our ${location.fields.name}, SEO Clients get Results`}
+            // smallTitle={`Area, We Cover in ${location.fields.name}`}
+            showContactBtn={false}
+          />
+        </div>
+      </section>
+
+      <section className="sectionPadding services">
+        <div className="container">
+          <SectionTopInfo
+            isCenter={true}
+            isFullWidth={true}
+            description={location.fields.desc4} // Pass desc here
+            title={`Are you Ready to take your ${location.fields.name} Business National and International?`}
+            smallTitle={`Here’s How it is Done`}
+            showContactBtn={false}
+          />
+        </div>
+      </section>
+
+      <section className="sectionPadding services">
+        <div className="container">
+          <SectionTopInfo
+            isCenter={true}
+            isFullWidth={true}
+            description={location.fields.desc5} // Pass desc here
+            title={`SEO Services ${location.fields.name}`}
+            smallTitle={`We Seek Traffic, Sales, and Growth for Your Business`}
+            showContactBtn={false}
+          />
+        </div>
+      </section>
     </>
   );
 }
--- common/SectionTopInfo.jsx
+++ common/SectionTopInfo.jsx
@@ -1,24 +1,27 @@
-'use client'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import React from 'react';
 import ContactUsButton from './ContactUsButton';
-import {useMounted} from "./../common/unmounted"
 
 
 const SectionTopInfo = ({ smallTitle, title, text, isCenter, isFullWidth, showContactBtn, description }) => {
-  const mounted = useMounted();
-  if(!mounted) return null;
-
+  
   const alignmentClass = isCenter ? 'text-center mx-auto' : '';
 
   return (
-    <div className={`sectionTopInfo  ${isFullWidth === true ? 'col-md-10' : 'col-md-6'} + ${alignmentClass}`} >
-      <h4 className='section-title-sm '>{smallTitle}</h4>
+    <div className={`sectionTopInfo ${isFullWidth ? 'col-md-10' : 'col-md-6'} ${alignmentClass}`}>
+      <h4 className='section-title-sm'>{smallTitle}</h4>
       <h2 className='title-xl fw-bold'>{title}</h2>
-      <div className='' dangerouslySetInnerHTML={{ __html: text }} />
-      <div dangerouslySetInnerHTML={{ __html: description }}></div>
-      {showContactBtn && (
-        <ContactUsButton />
-      )}
+      {/* <div dangerouslySetInnerHTML={{ __html: text }} /> */}
+
+      <div>
+        {description && typeof description === 'object' ? (
+          documentToReactComponents(description) // Render description as rich text
+        ) : (
+          <p></p> // Fallback message if description is not available
+        )}
+      </div>
+
+      {showContactBtn && <ContactUsButton />}
     </div>
   );
 };
