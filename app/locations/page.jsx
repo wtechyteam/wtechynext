de4ec12820c41168a27981c18353c1ef5f07b0bb
Wtechy <randeep@wtmails.com>
2024-09-25 12:28:15 +0530

    loc

--- app/locations/page.jsx
+++ app/locations/page.jsx
@@ -2,11 +2,12 @@ import InnerBanner from "../../common/InnerBanner";
 import SectionTopInfo from "../../common/SectionTopInfo";
 import Link from "next/link";
 import { fetchEntries } from "../contentful";
+import './locations.css'
 
 const contentTypes = {
-  usa: 'usaLocations',
-  canada: 'canadaLocations',
-  australia: 'australiaLocations'
+  usa: "usaLocations",
+  canada: "canadaLocations",
+  australia: "australiaLocations",
 };
 
 async function fetchAllLocations() {
@@ -17,7 +18,7 @@ async function fetchAllLocations() {
   return {
     usaItems: usaData.items || [],
     canadaItems: canadaData.items || [],
-    australiaItems: australiaData.items || []
+    australiaItems: australiaData.items || [],
   };
 }
 
@@ -26,13 +27,13 @@ export default async function LocationsFront() {
 
   return (
     <>
-    <head>
-    <title>Locations We Serve In</title>
-    {/* <meta title="Digital Marketing Strategies - Read Our Expert Insights"/>
+      <head>
+        <title>Locations We Serve In</title>
+        {/* <meta title="Digital Marketing Strategies - Read Our Expert Insights"/>
         <meta name="description"
         content="Find the latest insights into digital marketing strategies and trends on WTechy's blog. We reveal the secrets & best practices we use for our clients. Read more!"/>
        */}
-    </head>
+      </head>
       <InnerBanner title={"Locations We Serve In"} />
       <section className="sectionPadding bg-gray">
         <div className="container">
@@ -42,65 +43,49 @@ export default async function LocationsFront() {
             text={"Services we offer in the states of USA"}
           />
         </div>
-        <div className="Wrapper" style={{ display: "flex", gap: "2rem", marginLeft: "18rem", marginTop: "4rem" }}>
+        <div className="Wrapper location-wrapper">
           {usaItems.map((item) => (
             <section key={item.id}>
-              <div className="container">
-                <div className="row">
-                  <div className="col-md-6">
-                    <Link href={`${item.fields.slug}`} legacyBehavior>
-                      <a className="title-xl fw" style={{ textDecoration: "none", fontSize: "25px" }}>
-                        {item.fields.name}
-                      </a>
-                    </Link>
-                  </div>
-                </div>
+              <div className="location-item">
+                <Link href={`${item.fields.slug}`} legacyBehavior>
+                  <a className="location-link">{item.fields.name}</a>
+                </Link>
               </div>
             </section>
           ))}
         </div>
+
         <div className="container">
           <SectionTopInfo
             title={"Australia"}
             text={"Services we offer in the states of Australia"}
           />
         </div>
-        <div className="Wrapper" style={{ display: "flex", gap: "2rem", marginLeft: "18rem", marginTop: "4rem" }}>
+        <div className="Wrapper location-wrapper">
           {australiaItems.map((item) => (
             <section key={item.id}>
-              <div className="container">
-                <div className="row">
-                  <div className="col-md-6">
-                    <Link href={`${item.fields.slug}`} legacyBehavior>
-                      <a className="title-xl fw" style={{ textDecoration: "none", fontSize: "25px" }}>
-                        {item.fields.name}
-                      </a>
-                    </Link>
-                  </div>
-                </div>
+              <div className="location-item">
+                <Link href={`${item.fields.slug}`} legacyBehavior>
+                  <a className="location-link">{item.fields.name}</a>
+                </Link>
               </div>
             </section>
           ))}
         </div>
+
         <div className="container">
           <SectionTopInfo
             title={"Canada"}
             text={"Services we offer in the states of Canada"}
           />
         </div>
-        <div className="Wrapper" style={{ display: "flex", gap: "2rem", marginLeft: "18rem", marginTop: "4rem" }}>
+        <div className="Wrapper location-wrapper">
           {canadaItems.map((item) => (
             <section key={item.id}>
-              <div className="container">
-                <div className="row">
-                  <div className="col-md-6">
-                    <Link href={`${item.fields.slug}`} legacyBehavior>
-                      <a className="title-xl fw" style={{ textDecoration: "none", fontSize: "25px" }}>
-                        {item.fields.name}
-                      </a>
-                    </Link>
-                  </div>
-                </div>
+              <div className="location-item">
+                <Link href={`${item.fields.slug}`} legacyBehavior>
+                  <a className="location-link">{item.fields.name}</a>
+                </Link>
               </div>
             </section>
           ))}
